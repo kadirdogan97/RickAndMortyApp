bed763dfa0c4b8e227c7a40244ed6e3dd26f7881
kadirdogan <kadir.dogan@kgteknoloji.com>
2020-06-10 19:34:57 +0300

    added apollo graphql dependencies

--- app/src/main/java/com/kadirdogan97/rickandmortyapp/ui/MainActivity.kt
+++ app/src/main/java/com/kadirdogan97/rickandmortyapp/ui/MainActivity.kt
@@ -2,10 +2,33 @@ package com.kadirdogan97.rickandmortyapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.kadirdogan97.rickandmortyapp.FeedResultsQuery
+import com.kadirdogan97.rickandmortyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apolloClient = ApolloClient.builder()
+                .serverUrl("https://rickandmortyapi.com/graphql/")
+                .build()
+
+        apolloClient.query(FeedResultsQuery(1, "Rick","","","",""))
+                .enqueue(object : ApolloCall.Callback<FeedResultsQuery.Data>() {
+                    override fun onFailure(e: ApolloException) {
+                        Log.e("Apollo", "Error", e)
+                    }
+
+                    override fun onResponse(response: Response<FeedResultsQuery.Data>) {
+                        Log.e("Apollo", "Launch site: ${response.data?.characters.toString()}")
+                    }
+                })
+
     }
 }
