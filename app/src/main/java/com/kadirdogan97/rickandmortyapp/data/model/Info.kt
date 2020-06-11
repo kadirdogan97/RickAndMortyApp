294a9867a73441ba0e2deafb88d0f7895a0e67d4
kadirdogan <zkdr.dgn@gmail.com>
2020-06-11 12:17:19 +0300

    added models

---
+++ app/src/main/java/com/kadirdogan97/rickandmortyapp/data/model/Info.kt
@@ -0,0 +1,10 @@
+package com.kadirdogan97.rickandmortyapp.data.model
+
+/**
+ * Created by Kadir Doğan on 6/10/2020.
+ */
+data class Info(
+    val count: Int,
+    val pages: Int,
+    val next: Int
+)
--- app/src/main/java/com/kadirdogan97/rickandmortyapp/di/Module.kt
+++ app/src/main/java/com/kadirdogan97/rickandmortyapp/di/Module.kt
@@ -1,9 +1,51 @@
 package com.kadirdogan97.rickandmortyapp.di
 
+import android.app.Application
 import androidx.fragment.app.Fragment
+import androidx.room.Room
+import com.apollographql.apollo.ApolloClient
+import com.kadirdogan97.rickandmortyapp.AppConstants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Kadir Doğan on 6/10/2020.
  */
 
+val databaseModules = module {
+    fun provideDatabase(application: Application): Database{
+        return Room.databaseBuilder(
+            application.applicationContext,
+            Database::class.java,
+            "rickmorty"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    fun provideCharacterDao(database: Database):CharacterDao = database.getCharacterDao()
+
+    single { provideDatabase(application = androidApplication()) }
+    single { provideCharacterDao(database = get()) }
+}
+
+val networkModules = module{
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger.DEFAULT).setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .writeTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    fun provideApolloClient(okHttpClient: OkHttpClient): ApolloClient {
+        return ApolloClient.builder().okHttpClient(okHttpClient).serverUrl(AppConstants.BASE_GRAPHQL_URL).build()
+    }
+
+    single {provideOkHttpClient()}
+    single { provideApolloClient(okHttpClient = get()) }
+}
 
