df361c2b02a9b26e5e723c95ef515e3932a9965c
kadirdogan <zkdr.dgn@gmail.com>
2020-06-12 18:19:34 +0300

    recycler design and styles

---
+++ app/src/main/java/com/kadirdogan97/rickandmortyapp/ui/CharacterListAdapter.kt
@@ -0,0 +1,53 @@
+package com.kadirdogan97.rickandmortyapp.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.kadirdogan97.rickandmortyapp.R
+import com.kadirdogan97.rickandmortyapp.data.model.Character
+import com.kadirdogan97.rickandmortyapp.databinding.CharacterItemBinding
+
+/**
+ * Created by Kadir Doğan on 6/12/2020.
+ */
+class CharacterListAdapter: RecyclerView.Adapter<CharacterListAdapter.CharacterListItemViewHolder>() {
+
+    private var popularTvShows: MutableList<Character> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterListItemViewHolder {
+        val itemBinding = CharacterItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return CharacterListItemViewHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int = popularTvShows.size
+
+    override fun onBindViewHolder(holder: CharacterListItemViewHolder, position: Int) {
+        holder.bind(getCharacter(position))
+    }
+
+    private fun getCharacter(position: Int) = popularTvShows[position]
+
+    fun setCharacters(tvShows: List<Character>) {
+        val beforeSize = popularTvShows.size
+        popularTvShows.addAll(tvShows)
+        notifyItemRangeInserted(beforeSize, tvShows.size)
+    }
+
+    inner class CharacterListItemViewHolder(private val binding: CharacterItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(character: Character) {
+            with(binding) {
+                textViewCharacterName.text = character.name
+                textViewSpecies.text = character.species
+                textViewGender.text = character.gender
+                Glide.with(imageViewCharacterImage.context)
+                    .load(character.image)
+                    .into(imageViewCharacterImage)
+            }
+
+        }
+
+    }
+}
--- app/src/main/java/com/kadirdogan97/rickandmortyapp/ui/CharacterListFragment.kt
+++ app/src/main/java/com/kadirdogan97/rickandmortyapp/ui/CharacterListFragment.kt
@@ -1,11 +1,20 @@
 package com.kadirdogan97.rickandmortyapp.ui
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kadirdogan97.rickandmortyapp.R
+import com.kadirdogan97.rickandmortyapp.data.CharactersStatusViewState
+import com.kadirdogan97.rickandmortyapp.data.model.Character
+import com.kadirdogan97.rickandmortyapp.databinding.FragmentCharacterListBinding
+import com.kadirdogan97.rickandmortyapp.helper.EndlessScrollListener
+import com.kadirdogan97.rickandmortyapp.helper.observeNonNull
+import com.kadirdogan97.rickandmortyapp.helper.runIfNull
 import com.kadirdogan97.rickandmortyapp.viewmodel.VMCharacterList
 import com.kadirdogan97.rickandmortyapp.viewmodel.VMMain
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -15,12 +24,61 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
  */
 class CharacterListFragment : Fragment() {
     private val viewModel: VMCharacterList by viewModel()
+    private var _binding: FragmentCharacterListBinding? = null
+    private val binding get() = _binding!!
+
+    private val charactersAdapter = CharacterListAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_character_list, container, false)
+
+        _binding = FragmentCharacterListBinding.inflate(inflater, container, false)
+
+        viewModel.contents_.observeNonNull(this){
+            renderCharacters(it)
+        }
+        viewModel.status_.observeNonNull(this){
+            renderStatusResult(it)
+        }
+        savedInstanceState.runIfNull {
+            fetchCharacters(MainActivity.DEFAULT_PAGE)
+        }
+        initCharactersRecyclerView()
+        return binding.root
     }
 
+    fun fetchCharacters(page:Int){
+        viewModel.fetchCharacters(page)
+    }
+
+    private fun initCharactersRecyclerView() {
+        val linearLayoutManager = LinearLayoutManager(context)
+        binding.recyclerView.apply {
+            adapter = charactersAdapter
+            layoutManager = linearLayoutManager
+            addOnScrollListener(object : EndlessScrollListener(linearLayoutManager) {
+                override fun onLoadMore(page: Int) {
+                    fetchCharacters(page)
+                }
+            })
+        }
+    }
+
+    private fun renderCharacters(contents: List<Character>) {
+        charactersAdapter.setCharacters(contents)
+    }
+
+    private fun renderStatusResult(statusViewState: CharactersStatusViewState) {
+        binding.progressHorizontal.visibility = if (statusViewState.isLoading()) View.VISIBLE else View.GONE
+        binding.viewStateMessage.visibility = if (statusViewState.shouldShowErrorMessage()) View.VISIBLE else View.GONE
+        binding.viewStateMessage.text = statusViewState.getErrorMessage()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
