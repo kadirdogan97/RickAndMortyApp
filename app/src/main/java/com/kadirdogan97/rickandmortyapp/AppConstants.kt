79ae0ecc7878bb842211c1a1983f18b34cb57de3
kadirdogan <zkdr.dgn@gmail.com>
2020-06-12 14:00:49 +0300

    appconstants

---
+++ app/src/main/java/com/kadirdogan97/rickandmortyapp/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.kadirdogan97.rickandmortyapp
+
+/**
+ * Created by Kadir Doğan on 6/10/2020.
+ */
+object AppConstants {
+    const val BASE_GRAPHQL_URL = "https://rickandmortyapi.com/graphql"
+}
